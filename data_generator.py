32c701c2903cef4d79822e1ababbb99ba1ad7ca4
zhangtag <nowhitegive@foxmail.com>
2022-04-21 09:18:07 +0800

    预测时数据加入随机振荡

--- data_generator.py
+++ data_generator.py
@@ -10,4 +10,14 @@ x = np.empty((N, L), 'int64') # 10*100
 x[:] = np.array(range(L)) 
 x[:] += np.random.randint(-4 * T, 4 * T, N).reshape(N, 1)
 data = np.sin(x / 1.0 / T).astype('float64')
+
+# x = np.empty((N, L), 'float64')
+# x[:] =np.array(range(L)) 
+# x[:] += np.random.randint(-4 * T, 4 * T, N).reshape(N, 1) 
+# y = np.empty((N, L), 'float64')
+# y[:] = np.random.uniform(-0.1,0.1,size=(N,L)) 
+
+data = np.sin(x / 1.0 / T).astype('float64')
+# data += y[:]
+
 torch.save(data, open('train_raw_data.pt', 'wb'))
--- predict.py
+++ predict.py
@@ -76,9 +76,14 @@ if __name__ == '__main__':
     count = 0
     while count <= 0:
 
-        x = np.empty((N, L), 'int64')
-        x[:] = np.array(range(L)) + np.random.randint(-4 * T, 4 * T, N).reshape(N, 1) 
-        data = np.sin(x / 1.0 / T).astype('float64')
+        x = np.empty((N, L), 'float64')
+        x[:] =np.array(range(L)) 
+        x[:] += np.random.uniform(-4 * T, 4 * T, N).reshape(N, 1) 
+
+        y = np.empty((N, L), 'float64')
+        y[:] = np.random.uniform(-0.1,0.1,size=(N,L)) 
+
+        data = np.sin(x / 1.0 / T).astype('float64') + y[:]
         data = torch.from_numpy(data)
 
         # 通过设置 input_size 决定如何怎样预测：
